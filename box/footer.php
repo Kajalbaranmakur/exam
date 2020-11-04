0070884f1f658307005d3d2e6b238adf6441b922
Kajalbaranmakur <37069361+Kajalbaranmakur@users.noreply.github.com>
2020-11-05 02:20:54 +0530

    Add files via upload

---
+++ box/footer.php
@@ -0,0 +1,11 @@
+<!--Footer Starts Here-->
+        <footer class="footer">
+            <div class="wrapper">
+                <p>
+                    <a href="#" title="ABC Institutes">ABC Institute</a> &copy; <?php echo date('Y'); ?>. All Rights Reserved.    
+                    Powered by <a href="#" title="Kajal" target="_blank">Kajal Baran Makur</a>.
+                </p>
+            </div>
+        </footer>
+        <!--Footer Ends Here-->
+</html>
